46dc6e542944c083c6f2bf95a8aea2a7338b668b
wangyanjing01 <wangyanjing01@meicai.cn>
2019-07-11 15:35:18 +0800

    reimport cache

--- scope.go
+++ scope.go
@@ -13,9 +13,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/Vertamedia/chproxy/cache"
 	"github.com/Vertamedia/chproxy/config"
 	"github.com/Vertamedia/chproxy/log"
+	"github.com/nineteenthSpring/chproxy/cache"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
